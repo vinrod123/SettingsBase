6339546a5ea5fd8e5b76c09d8e70d53ad8c078a7
Vinícius Rodrigues <rodriguesviniciusoliveira@gmail.com>
2018-03-16 17:45:08 -0300

    first commit

---
+++ AppLearning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppLearning.Startup))]
+namespace AppLearning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
